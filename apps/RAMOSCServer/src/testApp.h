a561d724652fb8245ace27e06ebef779566ecd87
Motoi Shimizu <shimizu.motoi@gmail.com>
2013-04-28 02:25:38 +0900

    RAMOSCServer initital commit

---
+++ apps/RAMOSCServer/src/testApp.h
@@ -0,0 +1,42 @@
+//
+//  testApp.h
+//  RAMDanceToolkit
+//
+//  Created by motoishmz on 4/27/13.
+//  Copyright (c) 2013 YCAMInterlab. All rights reserved.
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxUI.h"
+#include "ofxOscSender.h"
+#include "ramOscPlaybacker.h"
+
+class testApp : public ofBaseApp
+{
+public:
+
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void load(const string path);
+	void setSpeed(const float new_speed);
+	
+	void dragEvent(ofDragInfo dragInfo);
+	void onValueChanged(ofxUIEventArgs &e);
+	
+	
+	vector<ramOscPlaybacker> playbackers;
+	ofxOscSender sender;
+	
+	ofxUICanvas *GUI;
+	ofxUITextInput *input_ip;
+	ofxUITextInput *input_port;
+	ofxUISlider *slider_speed;
+	
+	bool paused;
+	bool reset_playhead;
+	float speed;
+};
